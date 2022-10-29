52f9a42e55b1111fb733c1676c4ddfbc2a1f79b8
xssgit666 <qq14924@163.com>
2022-10-29 16:30:15 +0800

    last

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <!-- 路由的出口 -->
+    <keep-alive :include="cachePages">
+      <router-view/>
+    </keep-alive>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+export default {
+  name: 'App',
+  computed: {
+    ...mapState(['cachePages'])
+  }
+}
+</script>
+
+<style lang="less"></style>
